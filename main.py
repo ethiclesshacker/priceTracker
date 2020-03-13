9afd2caba60b0130f02f3b9a91d5f0c22cd45d5d
Aditya <mailingadityavs@gmail.com>
2020-03-13 20:26:26 +0530

    Added checking for GB/MB

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ def numify(data):
     except:
         return 0
 
+def numifyStorage(data):
+    data = data.upper()
+    try:
+        val = re.findall(r'\d+', data)[0]
+        if("GB" in data):
+            return val
+        if("MB" in val):
+            return (val/1024)
+    except:
+        return 0
+    return 0
 
 data = []
 data.append(["Company", "RAM", "Storage", "Front Camera", "Back Camera",
@@ -22,9 +33,10 @@ links = Flipkart.search("mobile phones")
 print("Links done...")
 
 count = 1
-
+c = 1
 for link in links:
-
+    print(c)
+    c = c + 1
     try:
         product = Flipkart.Product(link)
         product.getPrice()
@@ -34,8 +46,8 @@ for link in links:
         company = product.companyName
         frontCam = numify(product.feautures.get("Secondary Camera", "0"))
         backCam = numify(product.feautures.get("Primary Camera", "0"))
-        ram = numify(product.feautures.get("RAM", "0"))
-        storage = numify(product.feautures.get("Internal Storage", "0"))
+        ram = numifyStorage(product.feautures.get("RAM", "0"))
+        storage = numifyStorage(product.feautures.get("Internal Storage", "0"))
         battery = numify(product.feautures.get("Battery Capacity", "0"))
         price = product.price
         internet4G = 0
@@ -55,6 +67,6 @@ for link in links:
             continue
 
 
-with open("phoneDetails.csv", "w", newline="") as f:
+with open("phoneDetails2.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(data)
